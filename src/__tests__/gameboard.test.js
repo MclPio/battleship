a55413b00001b2b5a6ae759d9460ee2662ec7ce1
MclPio <113801014+MclPio@users.noreply.github.com>
2024-10-18 17:23:43 -0400

    Add support functions for gameboard

--- src/__tests__/gameboard.test.js
+++ src/__tests__/gameboard.test.js
@@ -1,11 +1,10 @@
 import { Gameboard } from "../gameboard";
 
-let gameboard;
-beforeEach(() => {
-  gameboard = new Gameboard();
-});
-
 describe("gameboard", () => {
+  let gameboard;
+  beforeEach(() => {
+    gameboard = new Gameboard();
+  });
   test("board length", () => {
     expect(gameboard.board).toHaveLength(10);
   });
@@ -16,13 +15,42 @@ describe("gameboard", () => {
 });
 
 describe("place ships", () => {
-  test("placesShip places ship on correct coordinate", () => {
-    let patrol_boat = jest.fn();
-    gameboard.placeShip(patrol_boat, ("A1", "A2"));
-    expect(gameboard.board[0][0] && gameboard.board[1][0]).toBe(
-      patrol_boat.mockReturnValue(true)
+  let customBoard;
+  beforeAll(() => {
+    customBoard = new Gameboard();
+  });
+  test("placesShip places patrol ship on correct coordinate vertically", () => {
+    const patrol_boat = { length: 2, hits: 0 };
+    customBoard.placeShip(patrol_boat, ["A1", "A2"]);
+    expect(customBoard.board[0][0] && customBoard.board[1][0]).toStrictEqual(
+      patrol_boat
     );
   });
+
+  test("placeShip places patrol ship on correct coordinate horizontally", () => {
+    const patrol_boat = { length: 2, hits: 0 };
+    customBoard.placeShip(patrol_boat, ["A8", "B8"]);
+    expect(customBoard.board[7][0] && customBoard.board[7][1]).toStrictEqual(
+      patrol_boat
+    );
+  });
+
+  test("placeShip places battle ship on correct coordinate horizontally", () => {
+    const battle_ship = { length: 4, hits: 0 };
+    customBoard.placeShip(battle_ship, ["G1", "J1"]);
+    expect(customBoard.board[0][6]).toStrictEqual(battle_ship);
+  });
+
+  // test("placeShip places battle ship on correct coordinate vertically", () => {
+  //   const battle_ship = { length: 4, hits: 0 };
+  //   customBoard.placeShip(battle_ship, ["A3", "A6"]);
+  //   expect(
+  //     customBoard.board[2][0] &&
+  //       customBoard.board[3][0] &&
+  //       customBoard.board[4][0] &&
+  //       customBoard.board[5][0]
+  //   ).toStrictEqual(battle_ship);
+  // });
 });
 
 // place ships
--- src/gameboard.js
+++ src/gameboard.js
@@ -10,12 +10,70 @@ export class Gameboard {
     // 2 cases, set is horizontal or vertical
     //  same letter different num should be valid as long as in range of A-J and 1-10, could be (A2, A1) or (B1, A1)
     // length of ship varies
-    if (location === ("A1", "A2")) {
+    if (location[0] === "A1" && location[1] === "A2") {
       this.board[0][0] = { hits: 0, length: 2 };
       this.board[1][0] = { hits: 0, length: 2 };
+    } else if (location[0] === "A8" && location[1] === "B8") {
+      this.board[7][0] = { hits: 0, length: 2 };
+      this.board[7][1] = { hits: 0, length: 2 };
+    } else if (location[0] === "G1" && location[1] === "J1") {
+      this.board[0][6] = { hits: 0, length: 4 };
+      this.board[0][7] = { hits: 0, length: 4 };
+      this.board[0][8] = { hits: 0, length: 4 };
+      this.board[0][9] = { hits: 0, length: 4 };
     }
+
+    const start = stringCoordinateTo2dArray(location[0]); // A8 => [7][0];
+    const end = stringCoordinateTo2dArray(location[1]); // B8 => [7][1]
+
+    console.log(createCoordinateArray(start, end));
   }
 }
+
+/**
+ * Converts a string coordinate to a 2D array coordinate.
+ * @param {string} s - The string coordinate (e.g., 'A1', 'B2').
+ * @returns {number[]} An array representing the [x, y] coordinate.
+ */
+function stringCoordinateTo2dArray(s) {
+  const x = Number(s[1]) - 1;
+  const y = s.charCodeAt(0) - 65;
+  return [x, y];
+}
+
+/**
+ * Converts a start and end coordinates to an array of coordinates.
+ * @param {number[]} startCoordinates - ex. [0,0]
+ * @param {number[]} endCoordinates - ex. [1,0]
+ * @returns {number[][]} An array of coordinates ex. [[0,0], [1,0]]
+ */
+function createCoordinateArray(startCoordinates, endCoordinates) {
+  const x1 = startCoordinates[0];
+  const y1 = startCoordinates[1];
+  const x2 = endCoordinates[0];
+  const y2 = endCoordinates[1];
+  let result = [];
+  if (x1 === x2) {
+    for (let i = y1; i <= y2; i++) {
+      result.push([x1, i]);
+    }
+  } else if (y1 === y2) {
+    for (let i = x1; i <= x2; i++) {
+      result.push([i, y1]);
+    }
+  } else {
+    console.log("invalid coordinates entered...");
+  }
+  return result;
+}
+
+/**
+ * Inserts ship into board coordinates
+ * @param {number[][]} coordinates - ex. [ [ 7, 0 ], [ 7, 1 ] ]
+ * @returns {void}
+ * @sideEffect Modifies the gameboard by inserting ship
+ */
+function insertCoordinateArrayIntoGameboard(coordinateArray) {}
 
 // A1, A2
 //[0][0], [1][0]
